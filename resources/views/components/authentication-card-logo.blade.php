30aced1241a19a6ade57d1283f11a2587420f336
gustavo.mfernandes <gustavo.mfernandes@fiocruz.br>
2025-05-08 21:22:20 -0300

    Acertando imagem empresa

--- resources/views/components/authentication-card-logo.blade.php
+++ resources/views/components/authentication-card-logo.blade.php
@@ -1,3 +1,3 @@
 <a href="/">
-    <img src="{{ asset('storage/' . $empresa->logo) }}" width="100" alt="Logo" class="brand-image "style="opacity: .8">
+    <img src="{{ isset($empresa->logo) ? asset('storage/' . $empresa->logo) : '#' }}" width="100" alt="Logo" class="brand-image "style="opacity: .8">
 </a>
