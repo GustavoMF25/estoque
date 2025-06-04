4ec56252e479823a7f1994ca8ef1dde9390c70d6
gustavo.mfernandes <gustavo.mfernandes@fiocruz.br>
2025-06-04 14:28:28 -0300

    app prodivder remove

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -20,6 +20,6 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
-        View::share('empresa', \App\Models\Empresa::first());
+        // View::share('empresa', \App\Models\Empresa::first());
     }
 }
