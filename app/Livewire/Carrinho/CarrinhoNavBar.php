c719c41ec34afaf3a9da522c0787f37db3b85f64
gustavo.mfernandes <gustavo.mfernandes@fiocruz.br>
2025-08-27 09:36:41 -0300

    #bug remover carrinho funcionando

--- app/Livewire/Carrinho/CarrinhoNavBar.php
+++ app/Livewire/Carrinho/CarrinhoNavBar.php
@@ -17,6 +17,31 @@ class CarrinhoNavBar extends Component
         $this->qtdItemCarrinho = array_sum(array_column($this->itens, 'quantidade'));
     }
 
+    public function removerItem(string $nome)
+    {
+        $carrinho = session('carrinho', []);
+
+        if (isset($carrinho[$nome])) {
+            unset($carrinho[$nome]);
+        }
+
+
+        $this->itens = $carrinho;
+        $this->qtdItemCarrinho = $this->calculaQtd($carrinho);
+
+        if (empty($carrinho)) {
+            return session()->forget('carrinho');
+        }
+
+        return session(['carrinho' => $carrinho]);
+    }
+    
+    private function calculaQtd(array $itens): int
+    {
+        if (empty($itens)) return 0;
+        return array_sum(array_map(fn($i) => (int)($i['quantidade'] ?? 0), $itens));
+    }
+
     public function render()
     {
         return view('livewire.carrinho.carrinho-nav-bar');
