114c256105667cee826abd74d76532500a6e3d61
gustavo.mfernandes <gustavo.mfernandes@fiocruz.br>
2025-11-04 15:25:37 -0300

    csrftoken except

--- app/Http/Middleware/VerifyCsrfToken.php
+++ app/Http/Middleware/VerifyCsrfToken.php
@@ -12,6 +12,6 @@ class VerifyCsrfToken extends Middleware
      * @var array<int, string>
      */
     protected $except = [
-        //
+        'api/mercadopago/webhook', // Adicione a rota do webhook aqui
     ];
 }
