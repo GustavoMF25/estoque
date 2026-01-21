0a147cf20c7b7e99b7cfac8168b02247833cfe0c
gustavo.mfernandes <gustavo.mfernandes@fiocruz.br>
2026-01-21 12:25:12 -0300

    Acesso liberado

--- app/Http/Middleware/BlockOldSalesModule.php
+++ app/Http/Middleware/BlockOldSalesModule.php
@@ -14,25 +14,25 @@ class BlockOldSalesModule
         $dataInicial = Carbon::parse('2026-01-15');
         $diasPermitidos = 5;
 
-        // ⏳ Se ainda não chegou 2026, ignora o middleware
-        if (Carbon::now()->lt($dataInicial)) {
-            return $next($request);
-        }
+        // // ⏳ Se ainda não chegou 2026, ignora o middleware
+        // if (Carbon::now()->lt($dataInicial)) {
+        //     return $next($request);
+        // }
 
-        // 📊 Calcula quantos dias se passaram e quantos faltam
-        $diasPassados = $dataInicial->diffInDays(Carbon::now());
-        $diasRestantes = max(0, $diasPermitidos - $diasPassados);
+        // // 📊 Calcula quantos dias se passaram e quantos faltam
+        // $diasPassados = $dataInicial->diffInDays(Carbon::now());
+        // $diasRestantes = max(0, $diasPermitidos - $diasPassados);
 
-        if ($diasPassados > $diasPermitidos) {
-            // Redireciona para página de aviso
-            return response()->view('modulo-vendas-expirado', [
-                'linkNovaVersao' => 'https://estoque.syntaxweb.com.br',
-                'dias' => $diasPassados,
-            ]);
-        }
+        // if ($diasPassados > $diasPermitidos) {
+        //     // Redireciona para página de aviso
+        //     return response()->view('modulo-vendas-expirado', [
+        //         'linkNovaVersao' => 'https://estoque.syntaxweb.com.br',
+        //         'dias' => $diasPassados,
+        //     ]);
+        // }
 
-        $mensagem = "⚠️ O módulo de vendas ficará disponível por mais {$diasRestantes} dia(s).";
-        session()->flash('error', $mensagem);
+        // $mensagem = "⚠️ O módulo de vendas ficará disponível por mais {$diasRestantes} dia(s).";
+        // session()->flash('error', $mensagem);
 
 
         return $next($request);
